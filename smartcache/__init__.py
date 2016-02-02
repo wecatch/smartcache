b579e9f59ce7595d739cba32394a9089d0b5d1cd
zhaoyongqiang <zhyq0826@gmail.com>
2016-02-02 16:50:58 +0800

    release 0.0.2

--- smartcache/__init__.py
+++ smartcache/__init__.py
@@ -26,5 +26,5 @@ from __future__ import absolute_import, division, print_function, with_statement
 # is zero for an official release, positive for a development branch,
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
-version = "0.0.1"
-version_info = (0, 0, 1, 100)
+version = "0.0.2"
+version_info = (0, 0, 2, 0)
